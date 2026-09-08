2b9ba943c9db8f791aff07754cb122aaec64a557
Atuarte <arthur.gdd2005@gmail.com>
2026-09-08 11:46:13 -0300

    Primeiro commit

---
+++ app/src/main/java/com/example/testedastelas/PainelInstrutorActivity.java
@@ -0,0 +1,14 @@
+package com.example.testedastelas;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PainelInstrutorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Conecta com o Layout que você já criou!
+        setContentView(R.layout.activity_painel_instrutor);
+    }
+}
