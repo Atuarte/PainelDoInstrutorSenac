8cd145edb97120de12bc901cbd0b627c79261e2b
Arthur <arthur.gdd2005@gmail.com>
2026-09-09 10:09:50 -0300

    Atualizado a barra de tarefas

--- app/src/main/java/com/example/testedastelas/PainelInstrutorActivity.java
+++ app/src/main/java/com/example/testedastelas/PainelInstrutorActivity.java
@@ -1,14 +1,33 @@
 package com.example.testedastelas;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class PainelInstrutorActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Conecta com o Layout que você já criou!
         setContentView(R.layout.activity_painel_instrutor);
+
+        // 1. Encontra o "Espaço Fantasma" que criamos no XML
+        View systemBarSpacer = findViewById(R.id.systemBarSpacer);
+
+        // 2. Faz o espaço fantasma crescer exatamente do tamanho da barra do celular
+        ViewCompat.setOnApplyWindowInsetsListener(systemBarSpacer, (view, windowInsets) -> {
+            Insets insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars());
+
+            // Pega os parâmetros da view e altera a altura.
+            ViewGroup.LayoutParams params = view.getLayoutParams();
+            params.height = insets.bottom;
+            view.setLayoutParams(params);
+
+            return windowInsets;
+        });
     }
 }
